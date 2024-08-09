bcd3ef0f95f2e420406cc532082d7ea99a7258b6
Emircan <emircandv787@gmail.com>
2024-08-09 21:41:28 +0300

    Add project files.

---
+++ AWS.SNS/Program.cs
@@ -0,0 +1,25 @@
+using Amazon.SimpleNotificationService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonSimpleNotificationService>();
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
